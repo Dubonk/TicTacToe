6119e427d7a7a9bd021003d4ba1db108af38fd42
kevin <kevindubon43@gmail.com>
2023-09-29 15:31:01 -0700

    Started filling out some basic css, trying to figure out if I should make a static gameboard in html or create it in js.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const gameContainer = document.querySelector('#gameContainer');
+
 const myGameboard = (() => {
   return { gameboard : ['X', 'O', 'O', 'X', 'O', 'X', 'X', 'O', 'O'],
 };
@@ -6,3 +8,15 @@ const myGameboard = (() => {
 const player = (name, marker) => {
     return {name, marker};
 }
+
+// function displayBoard() {
+//   const display = document.createElement('div');
+//   display.classList.add('gameBoard');
+//   display.innerText = myGameboard.gameboard;
+//   gameContainer.appendChild(display);
+// }
+// displayBoard();
+
+
+// In the Html, create 9 divs in gameContainer div, each of these 
+// divs will be its own tictactoe slot. Maybe use a table?
