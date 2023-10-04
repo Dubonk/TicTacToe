a907aa3fa231103625cdce4eb59ffa147b8d5a69
kevin <kevindubon43@gmail.com>
2023-10-04 16:50:08 -0700

    Removed all "progress" I made this week as it was not good code.

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-const gameContainer = document.querySelector('#gameContainer');
+let gameContainer = document.querySelector('.gameContainer');
 
 const myGameboard = (() => {
-  return { gameboard : ['X', 'O', 'O', 'X', 'O', 'X', 'X', 'O', 'O'],
-};
-})();
+  let gameboard = new Array(9);
+  return gameboard;
+  })();
 
 const player = (name, marker) => {
     return {name, marker};
 }
 
-// function displayBoard() {
-//   const display = document.createElement('div');
-//   display.classList.add('gameBoard');
-//   display.innerText = myGameboard.gameboard;
-//   gameContainer.appendChild(display);
-// }
-// displayBoard();
-
-
 // In the Html, create 9 divs in gameContainer div, each of these 
 // divs will be its own tictactoe slot. Maybe use a table?
+
