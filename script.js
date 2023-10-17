791a88540916decb591c906e1aa5dea2cc72c200
kevin <kevindubon43@gmail.com>
2023-10-17 16:14:04 -0700

    Cleaned dead code, added name inputs.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let gameContainer = document.querySelector('#game');
+let gameContainer = document.querySelector('#game'); 
 
 let myGameboard = (() => {
   let gameboard = new Array(9);
@@ -8,17 +8,3 @@ let myGameboard = (() => {
 const player = function (name, marker) {
     return {name, marker};
 }
-
-const player1 = new player('Player 1', 'X');
-const player2 = new player('Player 2', 'O');
-
-
-function displayBoard() {
-  for (let i = 0; i > gameboard.length; i++) {
-    let cell = gameboard[i];
-    cell.classList.append('cell');
-    gameContainer.appendChild(cell);
-  }
-};
-
-displayBoard();
