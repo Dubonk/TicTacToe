5bf36bfd21ecd86275bebcf2dc238aca94675198
kevinLaptop <kevindubon43@gmail.com>
2023-10-26 15:06:36 -0700

    Working on js section, added array through dom. removed cell divs in html.

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 
 const myGameboard = (() => {
-  let gameContainer = document.querySelector('#game'); 
-  let gameboard = new Array(9);
+  const gameContainer = document.getElementById('game');
+  let gameboard = ['x', 'o', 'o', 'x', 'x', 'o', 'o', 'x', 'o'];
+  gameboard.forEach((item) => {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.innerText = item;
+    gameContainer.append(cell);
+    cell.addEventListener('click', handleClick, {once: true});
+  })
   return gameboard;
   })();
 
-const player = function (name, marker) {
-    name, marker;
+function createPlayer (name, marker) {
+   return {name, marker};
 }
+
+const firstPlayer = createPlayer(name || "player1", 'x');
+const secondPlayer = createPlayer(name || 'Player 2', 'O');
+
+
+function handleClick(e) {
+  placeMark();
+  checkWin();
+  checkTie();
+  switchTurn();
+}
+function placeMark() {
+
+}
+function checkWin() {
+
+}
+function checkTie() {
+
+}
+
+//console.log(firstPlayer);
+// console.log(secondPlayer);
+//console.log(myGameboard);
