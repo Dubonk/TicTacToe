7d346e35c655497973296fb69b782f574f1b3dc2
kevinLaptop <kevindubon43@gmail.com>
2023-10-27 14:07:06 -0700

    Restarting js, code was too messy

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
-let gameboard = ['','','','','','','','','',];
 
-const myGameboard = (() => {
-  const gameContainer = document.getElementById('game');
-  gameboard.forEach((item) => {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.innerText = item;
-    gameContainer.append(cell);
-    cell.addEventListener('click', handleClick, {once: true})
-
-    // cell.addEventListener('click', () => {
-      
-    //   handleClick();
-    //   console.log(myGameboard);
-    // });
-  })
-  return gameboard;
-  })();
 
 function createPlayer (name, marker) {
    return {name, marker};
 }
-
-const firstPlayer = createPlayer(name || "player1", 'x');
-const secondPlayer = createPlayer(name || 'Player 2', 'O');
-
 
 function handleClick() {
   placeMark();
@@ -32,10 +10,9 @@ function handleClick() {
   checkWin();
   checkTie();
   switchTurn();
-  console.log(myGameboard);
 }
 function placeMark() {
-  
+
 }
 function updateBoard() {
 
