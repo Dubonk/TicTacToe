28d82d401c74f0b9ba29cbcf94fd4e91589e097f
kevinLaptop <kevindubon43@gmail.com>
2023-10-26 17:23:04 -0700

    Set up functions no logic.

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+let gameboard = ['','','','','','','','','',];
 
 const myGameboard = (() => {
   const gameContainer = document.getElementById('game');
-  let gameboard = ['x', 'o', 'o', 'x', 'x', 'o', 'o', 'x', 'o'];
   gameboard.forEach((item) => {
     const cell = document.createElement('div');
     cell.classList.add('cell');
     cell.innerText = item;
     gameContainer.append(cell);
-    cell.addEventListener('click', handleClick, {once: true});
+    cell.addEventListener('click', handleClick, {once: true})
+
+    // cell.addEventListener('click', () => {
+      
+    //   handleClick();
+    //   console.log(myGameboard);
+    // });
   })
   return gameboard;
   })();
@@ -20,13 +26,18 @@ const firstPlayer = createPlayer(name || "player1", 'x');
 const secondPlayer = createPlayer(name || 'Player 2', 'O');
 
 
-function handleClick(e) {
+function handleClick() {
   placeMark();
+  updateBoard();
   checkWin();
   checkTie();
   switchTurn();
+  console.log(myGameboard);
 }
 function placeMark() {
+  
+}
+function updateBoard() {
 
 }
 function checkWin() {
@@ -35,7 +46,11 @@ function checkWin() {
 function checkTie() {
 
 }
+function switchTurn() {
+
+}
 
 //console.log(firstPlayer);
-// console.log(secondPlayer);
+//console.log(secondPlayer);
 //console.log(myGameboard);
+//console.log(cell);
